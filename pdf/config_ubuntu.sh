da1816e5fb294557a6b5af8bc7affa1fcbd8232d
MaxG175 <maxg175@yandex.ru>
2024-07-19 06:43:38 -0400

    commited

--- pdf/config_ubuntu.sh
+++ pdf/config_ubuntu.sh
@@ -1,5 +1,7 @@
-export CXX="g++"
-export CC="gcc"
+export CXX="afl-c++"
+export CC="afl-cc"
+export AFL_USE_ASAN=1
+export AFL_USE_UBSAN=1
 export CFLAGS="-I/usr/include/freetype2 -I/usr/include -g -O0 --coverage -fsanitize=address"
 export CXXFLAGS="-I/usr/include/freetype2 -I/usr/include -g -O0 --coverage -fsanitize=address"
 ./configure
